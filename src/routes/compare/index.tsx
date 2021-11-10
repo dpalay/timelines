2871ec829ee13b10f9755b00c916148c7a7e956d
dpalay <lordthanda@gmail.com>
2021-11-09 23:50:54 -0600

    logic for winning/losing and resetting

--- src/routes/compare/index.tsx
+++ src/routes/compare/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionalComponent, h } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import { data } from "../../assets/data";
 import { Difficulties, EventCategories, TimeEvent } from "../../Model";
 import style from "./style.css";
@@ -11,6 +11,9 @@ interface Props {
 
 const Compare: FunctionalComponent<Props> = (props) => {
 
+    const event1ref = useRef<HTMLDivElement | undefined>(null);
+    const event2ref = useRef<HTMLDivElement | undefined>(null);
+    const [dirty, setDirty] = useState(false);
  
   const [event1, setEvent1] = useState<TimeEvent>({
     name: "",
@@ -24,7 +27,9 @@ const Compare: FunctionalComponent<Props> = (props) => {
   });
   const [correctAnswer, setCorrectAnswer] = useState<string>("");
   const [selectedAnswer, setSelectedAnswer] = useState<string>("");
+
   useEffect(() => {
+    setDirty(() => false);
     console.log(props);
     const filteredList = data.filter((x) =>
       x.categories.find((y) => y & props.category)
@@ -40,29 +45,41 @@ const Compare: FunctionalComponent<Props> = (props) => {
       setEvent2(e2);
         setCorrectAnswer(e1.date < e2.date ? e1.name : e2.name);
     }
-  }, [props]);
+  }, [props, dirty]);
 
 
   const makeGuess: h.JSX.EventHandler<h.JSX.TargetedMouseEvent<HTMLDivElement>> = (event): void => {
-    const divs = document.getElementsByClassName(style.guessed)
-    divs.item(0)?.classList.remove(style.guessed);
-    divs.item(1)?.classList.remove(style.guessed);
-
+    event1ref.current?.classList.remove(style.guessed);
+    event2ref.current?.classList.remove(style.guessed);
     const target = event.target as HTMLDivElement;
     target.classList.add(style.guessed)
     setSelectedAnswer(target.innerHTML);
 }
 
 const submitGuess: h.JSX.EventHandler<h.JSX.TargetedMouseEvent<HTMLInputElement>> = (event): void => {
-
+if (correctAnswer === selectedAnswer) {
+  alert(`Correct!
+${event1.name} was on ${event1.date.toLocaleDateString()}  
+${event2.name} was on ${event2.date.toLocaleDateString()}`);
+}
+else {
+    alert(`Incorrect!
+${event1.name} was on ${event1.date.toLocaleDateString()}  
+${event2.name} was on ${event2.date.toLocaleDateString()}`);
+}
+event1ref.current?.classList.remove(style.guessed);
+event2ref.current?.classList.remove(style.guessed);
+setSelectedAnswer("");
+setCorrectAnswer("");
+setDirty(true);
 }
 
   return (
     <div>
       <h1>Compare</h1>
-      <div id="event1" onClick={makeGuess}>{event1.name}</div>
+      <div ref={event1ref} id="event1" onClick={makeGuess}>{event1.name}</div>
       <div id="vs">vs.</div>
-      <div id="event2" onClick={makeGuess}>{event2.name}</div>
+      <div ref={event2ref} id="event2" onClick={makeGuess}>{event2.name}</div>
       <input type="button" value="Submit" onClick={submitGuess} />
     </div>
   );
